3681d31b223930291940093f3f3847580bce25aa
SKKYWEB <74390833+SKKYWEB@users.noreply.github.com>
2020-11-13 10:37:39 +0800

    origin

---
+++ precache-manifest.fbaad14ee5f121fc6728e7648bed90de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a5b30e248d567f100c4e7ed8dacadb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db966c01d53feb847f64",
+    "url": "./static/css/main.f8cbf4c7.chunk.css"
+  },
+  {
+    "revision": "87d7ab7c9d89a7d97852",
+    "url": "./static/js/2.f1732f4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.f1732f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db966c01d53feb847f64",
+    "url": "./static/js/main.fb3fb235.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "7bfaa54f2f8e0873930aa3b519db145f",
+    "url": "./static/media/home1.7bfaa54f.jpg"
+  },
+  {
+    "revision": "4ff7344f81890d3b58b4768b6e696907",
+    "url": "./static/media/home2.4ff7344f.jpg"
+  },
+  {
+    "revision": "09c81d85a2c12c7bdd306ed56acda3b3",
+    "url": "./static/media/home3.09c81d85.jpg"
+  },
+  {
+    "revision": "c65e9dfcfdcd960a02d1d61c216f9ab8",
+    "url": "./static/media/kansei.c65e9dfc.jpg"
+  }
+]);
